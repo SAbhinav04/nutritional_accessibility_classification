0c9af4c5ef08fc0db3ca21ce9572de6ce37054be
shifa172004 <shifafirdose17@gmail.com>
2026-03-11 22:22:52 +0530

    Added preprocessing script

---
+++ dataset/preprocessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+food=pd.read_csv("food_dataset.csv")
+
+print(food.shape)
+print(food.info)
+print(food.head())
+print(food.columns)
+print(food.isnull().sum())
+print(food.describe())
+print(food["meal_category"].value_counts())
